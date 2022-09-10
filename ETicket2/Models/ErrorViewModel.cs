3d9e459752ec3c37020a0ebf54e3b63bafaf29d9
Abhishek Saxena <rpa.abhishek@gmail.com>
2022-09-10 03:11:44 -0400

    Add project files.

---
+++ ETicket2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ETicket2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
